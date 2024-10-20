c5a5c930433eeb70e8928b81e4767f9c287599dd
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2024-10-19 19:57:08 -0700

    Initial Commit

---
+++ OrtizSnapchat4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrtizSnapchat4
+//
+//  Created by mauro on 10/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
