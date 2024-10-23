a39aa97a1b3d1c2b6be47497051c81d66d97bafe
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2024-10-23 08:39:18 -0700

    Lab12 PrimerAvance

---
+++ OrtizSnapchat4/SnapsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SnapsViewController.swift
+//  OrtizSnapchat4
+//
+//  Created by mauro on 10/23/24.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- OrtizSnapchat4/iniciarSesionViewController.swift
+++ OrtizSnapchat4/iniciarSesionViewController.swift
@@ -19,10 +19,20 @@ class iniciarSesionViewController: UIViewController {
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {(user, error) in
             print("Intentando Iniciar Sesion")
-            if error != nil{
-                print("Se presento el siguiente error: \(error)")
-            }else{
-                print("Inicio se sesion exitoso")
+            if error != nil {
+                print("Se presentó el siguiente error: \(error)")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("Intentando crear un usuario")
+                    if error != nil {
+                        print("Se presentó el siguiente error al crear el usuario: \(error)")
+                    } else {
+                        print("El usuario fue creado exitosamente")
+                        self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                    }
+                })
+            } else {
+                print("Inicio de Sesión Exitoso")
+                self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
             }
         }
     }
